f7139453159d558b8af2c42903d92acfc569eab2
Rahul Roy <techniciablog@gmail.com>
2019-03-02 16:18:15 +0530

    Add bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import ListGroup from 'react-bootstrap/ListGroup'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
 import logo from './logo.svg';
 import './App.css';
 
 // Scrimba exercises
-import InlineStylesWithStyleProperty from './InlineStylesWithStyleProperty'
+import InlineStylesWithStyleProperty from './InlineStylesWithStyleProperty';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Link to="/InlineStylesWithStyleProperty/">InlineStylesWithStyleProperty</Link>
+          <Jumbotron>
+            <Container>
+              <Row>
+                <Col>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <Link to="/InlineStylesWithStyleProperty/">InlineStylesWithStyleProperty</Link>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Col>
+              </Row>
+            </Container>
+          </Jumbotron>
+
           <Route path="/InlineStylesWithStyleProperty" exact component={InlineStylesWithStyleProperty} />
         </div>
       </Router>
